f99c420b2fe652c73b4d0706a448a13470b64398
Sofia Gonzalez <sgonzalezbodello@gmail.com>
2022-07-18 09:36:39 -0300

    docs: Scrips de Postman y BBDD
    Se incoorporan los documentos relacionados para ejecutar las consultas
    de postan. Ademas se suma el scrip para completar la BBDD

---
+++ datosBBDDHoroscopos.sql
@@ -0,0 +1,43 @@
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Aries', '21-03-2000', '19-04-2000', 'ARIES');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Tauro', '20-04-2000', '20-05-2000', 'TAURO');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Cancer', '21-06-2000', '22-07-2000', 'CANCER');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Leo', '23-07-2000', '22-08-2000', 'LEO');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Virgo', '23-08-2000', '22-09-2000', 'VIRGO');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Libra', '23-09-2000', '22-10-2000', 'LIBRA');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Escorpio', '23-10-2000', '21-11-2000', 'ESCORPIO');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Sagitario', '22-11-2000', '21-12-2000', 'SAGITARIO');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Capricornio', '22-12-2000', '20-01-2000', 'CAPRICORNIO');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Acuario', '21-01-2000', '19-12-2000', 'ACUARIO');
+
+INSERT INTO public.horoscopo
+(descripcion, fecha_final, fecha_inicial, tipo)
+VALUES('Soy Piscis', '20-02-2000', '20-03-2000', 'PISCIS');
